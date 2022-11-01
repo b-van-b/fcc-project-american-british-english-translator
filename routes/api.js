b423dd6f021a3fe2cc9d644f043c4b139fa04131
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-11-01 16:06:01 +0000

    feat(api): route to translator

--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,7 @@ const Translator = require("../components/translator.js");
 module.exports = function (app) {
   const translator = new Translator();
 
-  app.route("/api/translate").post((req, res) => {});
+  app.route("/api/translate").post((req, res) => {
+    res.json(translator.translate(req.body.text, req.body.locale));
+  });
 };
