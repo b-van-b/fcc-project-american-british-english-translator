e783290a6e984893743b60b434f5f5654d97e7ba
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-11-01 16:47:03 +0000

    chore(translator): add dummy translate function

--- components/translator.js
+++ components/translator.js
@@ -3,6 +3,10 @@ const americanToBritishSpelling = require("./american-to-british-spelling.js");
 const americanToBritishTitles = require("./american-to-british-titles.js");
 const britishOnly = require("./british-only.js");
 
-class Translator {}
+class Translator {
+  translate(text, locale) {
+    return text;
+  }
+}
 
 module.exports = Translator;
