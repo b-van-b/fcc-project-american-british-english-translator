b212def56179c0b41577e17b4bf327ca63fe163b
b-van-b <115096508+b-van-b@users.noreply.github.com>
2022-11-01 15:54:44 +0000

    chore: debug api requests to console

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// report all requests to console
+app.use("/", (req, res, next) => {
+  console.log(`\n${req.method} ${req.path} - ${req.ip}`);
+  console.log(req.body);
+  next();
+});
+
 // Index page (static HTML)
 app.route('/')
   .get(function (req, res) {
